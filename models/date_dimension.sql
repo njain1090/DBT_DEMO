4ff763e64dfd4e9c91181a930714e7577f235efc
Neha <jain.neha0020@gmail.com>
2026-02-27 19:05:26 +0000

    Daily weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -5,16 +5,8 @@ to_timestamp(started_at) AS STARTED_AT,
 DATE(to_timestamp(started_at)) AS DATE_STARTED_AT,
 HOUR(to_timestamp(started_at)) AS HOUR_STARTED_AT,
 dayname(to_timestamp(started_at)),
-CASE 
-WHEN dayname(to_timestamp(started_at)) in ('Sat','Sun')
-THEN 'WEEKEND'
-ELSE 'BusinessDay'
-END AS DAY_TYPE,
-CASE WHEN 
-MONTH(to_timestamp(started_at)) in (12,1,2) THEN 'WINTER'
-WHEN MONTH(to_timestamp(started_at)) in (3,4,5) THEN 'SPRING'
-WHEN MONTH(to_timestamp(started_at)) in (6,7,8) THEN 'AUTUMN'
-ELSE 'Summer' END AS STATION_OF_YEAR
+{{day_type('started_at')}} AS DAY_TYPE,
+{{getseason('started_at')}} AS STATION_OF_YEAR
 
 from 
 {{ source('demo', 'bike') }}
